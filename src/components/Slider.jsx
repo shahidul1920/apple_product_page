dbfaf5caa1d050eed0cdfc1515daec39913d1ae7
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-03-21 20:49:03 +0600

    animation added

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,14 @@
+import { useAnimation, useInView } from "motion/react";
 import React, { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 
 const Slider = () => {
   const images = ["./01.jpg", "./1.jpg", "./2.jpg"];
   const [current, setCurrent] = useState(0);
   const slideRef = useRef(null);
+  const isInViewPort = useInView(slideRef);
+  const animetnControl = useAnimation()
+
 
   const nextSlide = () => {
     setCurrent(current === images.length - 1 ? 0 : current + 1);
@@ -13,17 +18,20 @@ const Slider = () => {
   }
 
   useEffect(()=>{
+    if(isInViewPort){
+      animetnControl.start('on')
+    }
     if(slideRef.current){
       slideRef.current.style.transform = `translateX(-${current * 100}%)`;
     }
-  },[current]);
+  },[current, isInViewPort]);
 
   return (
     <div className="container mx-auto my-20">
       <h1 className="text-6xl font-bold text-left my-20">
         Get the highlights.
       </h1>
-      <section className="overflow-hidden relative rounded-2xl">
+      <motion.section initial='off' variants={{off: {opacity:0, x:-100}, on: {opacity:1, x:0}}} animate={animetnControl} transition={{duration:1, ease: 'easeInOut'}} className="overflow-hidden relative rounded-2xl">
         <div ref={slideRef} className="flex h-[700px] transition-transform duration-1000 ease-in-out">
           {images.map((image, i) => (
             <div
@@ -38,14 +46,14 @@ const Slider = () => {
             </div>
           ))}
         </div>
-      </section>
+      </motion.section>
       <div className="slider-nav flex items-center justify-center p-8 rounded-full bg-[#FBF7F4] max-w-[230px] mx-auto gap-5 my-6">
         {images.map((_, i) => (
           <div
             key={i}
             onClick={() => setCurrent(i)}
-            className={`slider-nav-item size-[14px] bg-[#292A2E] rounded-full block ${
-              current === i ? "bg-[#FFD302]" : ""
+            className={`slider-nav-item size-[14px] bg-[#57585e] rounded-full block ${
+              current === i ? "bg-slate-700" : ""
             }`}
           />
         ))}
